8a40c1811e60a7603480fb74353616b7e6213ceb
Freewind <nowindlee@gmail.com>
2020-05-15 17:53:51 +0800

    fix an issue

--- README.md
+++ README.md
@@ -3,6 +3,10 @@ TypeScript Node "request" Demo
 
 使用"request"这个库来发送http请求。
 
+注意：一定要添加`Connection: "keep-alive"`这个header，否则经常会得到奇怪的socket hang up错误
+
+https://github.com/request/request/issues/2047#issuecomment-272473278
+
 ```
 npm install
 npm run demo
--- hello.ts
+++ hello.ts
@@ -1,5 +1,10 @@
 import request, {Response} from 'request';
 
-request("https://github.com/", (error: any, response: Response, body: any) => {
+request("https://github.com/", {
+  headers: {
+    // https://github.com/request/request/issues/2047#issuecomment-272473278
+    Connection: "keep-alive"
+  }
+}, (error: any, response: Response, body: any) => {
   console.log(body);
 })
